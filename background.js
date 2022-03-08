5be3f4864fb4d86405b576107c1e333475f5ea9d
Quentin Helvie <73959954+helvieq499@users.noreply.github.com>
2022-03-08 09:16:15 -0500

    Create background.js

---
+++ background.js
@@ -0,0 +1,26 @@
+cache = {};
+
+// note: these 2 event handlers can be merge for perf
+// [setter-guid, section, values]
+window.addEventListener("message", event => {
+    if (!Array.isArray(event.data) 
+        || event.data.length != 3 
+        || event.data[0] != "1bc85e29-1f2d-462e-9e09-72fa5eb99b34") return;
+    cache[event.data[1]] = event.data[2];
+    localStorage.setItem(event.data[1], JSON.stringify(event.data[2]));
+});
+
+// [getter-guid, section]
+window.addEventListener("message", event => {
+    if (!Array.isArray(event.data) 
+        || event.data.length != 2 
+        || event.data[0] != "c62f7e27-4d05-402c-8ad2-8f47a0105614") return;
+    event.source.postMessage(getValue(event.data[1]), event.origin);
+});
+
+function getValue(section) {
+    let val = cache[section];
+    if (!val) 
+        return cache[section] = JSON.parse(localStorage.getItem(section));
+    else return val;
+}
