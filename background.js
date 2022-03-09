4af28a4f6834d9cc53ee5e34ba3cddc8d1e5f7ac
Quentin Helvie <73959954+helvieq499@users.noreply.github.com>
2022-03-09 09:57:03 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 cache = {};
 
-// note: these 2 event handlers can be merge for perf
-// [setter-guid, section, values]
+// note: these event handlers can be merge for perf
+
+// section setter
+// [guid, section, values]
 window.addEventListener("message", event => {
     if (!Array.isArray(event.data) 
         || event.data.length != 3 
@@ -10,12 +12,36 @@ window.addEventListener("message", event => {
     localStorage.setItem(event.data[1], JSON.stringify(event.data[2]));
 });
 
-// [getter-guid, section]
+cache = {};
+
+// section key setter
+// [guid, section, key, value]
+window.addEventListener("message", event => {
+    if (!Array.isArray(event.data) 
+        || event.data.length != 3 
+        || event.data[0] != "b4fb68dc-6c27-49d5-b39c-610261751bdc") return;
+    if (!getValue(event.data[1]))
+        cache[event.data[1]] = {};
+    cache[event.data[1]][event.data[2]] = event.data[3];
+    localStorage.setItem(event.data[1], JSON.stringify(event.data[2]));
+});
+
+// section getter
+// [guid, section]
 window.addEventListener("message", event => {
     if (!Array.isArray(event.data) 
         || event.data.length != 2 
         || event.data[0] != "c62f7e27-4d05-402c-8ad2-8f47a0105614") return;
     window.parent.postMessage(getValue(event.data[1]), "*");
+});
+
+// section key getter
+// [guid, section, key]
+window.addEventListener("message", event => {
+    if (!Array.isArray(event.data) 
+        || event.data.length != 2 
+        || event.data[0] != "c62f7e27-4d05-402c-8ad2-8f47a0105614") return;
+    window.parent.postMessage(getValue(event.data[1])[event.data[2]], "*");
 });
 
 function getValue(section) {
