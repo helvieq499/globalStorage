0901972c74c9f7fedca900f4f0ad612fe894944d
Quentin Helvie <73959954+helvieq499@users.noreply.github.com>
2022-03-10 09:12:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 globalStorage = {
-  __iframe,
+  __promises: [],
+  __iframe: undefined,
+  getSection: section => {
+    let promise = new Promise((res, rej) => globalStorage.__promises.push([res, rej]));;
+    i.contentWindow.postMessage(["c62f7e27-4d05-402c-8ad2-8f47a0105614", section], "*");
+    return promise;
+  }
 }
 
 let i = document.createElement("iframe");
-i.src = "https://helvie499.github.io/globalStorage";
+i.src = "https://helvieq499.github.io/globalStorage";
 i.style.display = "none";
 document.head.appendChild(i);
-global.__iframe = i
+globalStorage.__iframe = i
+
+window.addEventListener("message", event => {
+    if (event.origin != "https://helvieq499.github.io") return;
+    globalStorage.__promises.shift()[0](event.data);
+});
