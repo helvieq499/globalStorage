4b36b95ca8b45379ed576b89879c7538aba90635
Quentin Helvie <73959954+helvieq499@users.noreply.github.com>
2022-03-11 09:12:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 globalStorage = {
   __promises: [],
   __iframe: undefined,
-  getSection: section => {
-    let promise = new Promise((res, rej) => globalStorage.__promises.push([res, rej]));;
-    i.contentWindow.postMessage(["c62f7e27-4d05-402c-8ad2-8f47a0105614", section], "*");
+  __sendMessage: (values, resp) => {
+    let promise = undefined;
+    if (resp)
+      promise = new Promise((res, rej) => globalStorage.__promises.push([res, rej]));;
+    i.contentWindow.postMessage(values, "*");
     return promise;
-  }
+  },
+  get: section => globalStorage.__sendMessage(["c62f7e27-4d05-402c-8ad2-8f47a0105614", section], true),
+  set: (section, value) => globalStorage.__sendMessage(["1bc85e29-1f2d-462e-9e09-72fa5eb99b34", section, value]),
 }
 
 let i = document.createElement("iframe");
@@ -15,6 +19,6 @@ document.head.appendChild(i);
 globalStorage.__iframe = i
 
 window.addEventListener("message", event => {
-    if (event.origin != "https://helvieq499.github.io") return;
-    globalStorage.__promises.shift()[0](event.data);
+  if (event.origin != "https://helvieq499.github.io") return;
+  globalStorage.__promises.shift()[0](event.data);
 });
